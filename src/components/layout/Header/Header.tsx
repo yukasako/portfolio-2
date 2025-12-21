3d6e67465d309f954ecbc731910688b6d0a05f75
yukasako <yukainsweden@gmail.com>
2025-12-21 20:19:51 +0900

    Update Navbar and Header.

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -3,15 +3,13 @@ import styles from './Header.module.css';
 
 function Header() {
   return (
-    <>
-      <header className={styles.header}>
-        <h1>
-          Yukako Sakozono <br />
-          Isacson
-        </h1>
-        <Navbar></Navbar>
-      </header>
-    </>
+    <header className={styles.header}>
+      <h1>
+        Yukako Sakozono <br />
+        Isacson
+      </h1>
+      <Navbar></Navbar>
+    </header>
   );
 }
 
--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBrush } from '@fortawesome/free-solid-svg-icons';
 import styles from './Navbar.module.css';
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => setOpen((v) => !v);
+  const close = () => setOpen(false);
+
   return (
     <nav className={styles.navbar}>
-      <FontAwesomeIcon icon={faBrush} className={styles.hamburger} />
+      <FontAwesomeIcon
+        icon={faBrush}
+        className={styles.hamburger}
+        onClick={toggle}
+        aria-label='Open menu'
+      />
 
-      <div className={styles.navbarLinks}>
+      <div
+        className={`${styles.navbarLinks} ${open ? '' : styles.displayNone}`}
+        onClick={close}
+      >
         <a href='#introduction'>About</a>
         <a href='#skills'>Abilities</a>
         <a href='#projects'>Projects</a>
